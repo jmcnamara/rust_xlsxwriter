1ba21e406c9284f19f0ce0cbab513ccd809f90e9
John McNamara <jmcnamara@cpan.org>
2023-01-17 14:05:23 +0000

    docs: fix minor issue

--- src/worksheet.rs
+++ src/worksheet.rs
@@ -4628,13 +4628,13 @@ impl Worksheet {
     /// #
     /// # fn main() -> Result<(), XlsxError> {
     /// #     let mut workbook = Workbook::new();
-    ///
-    ///     let worksheet = workbook.add_worksheet();
-    ///     worksheet.write_string_only(100, 100, "Test")?;
-    ///
+    /// #
+    /// #     let worksheet = workbook.add_worksheet();
+    /// #     worksheet.write_string_only(100, 100, "Test")?;
+    /// #
     ///     // Set a page break at rows 20, 40 and 60.
     ///     worksheet.set_page_breaks(&[20, 40, 60])?;
-    ///
+    /// #
     /// #     workbook.save("worksheet.xlsx")?;
     /// #
     /// #     Ok(())
