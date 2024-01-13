94fbb66621e9ff71a274baa937c0866c191b14df
John McNamara <jmcnamara@cpan.org>
2024-01-13 09:06:44 +0000

    serde: add XlsxSerialize examples

---
+++ examples/doc_xlsxserialize_num_format.rs
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+//
+// Copyright 2022-2024, John McNamara, jmcnamara@cpan.org
+
+//! Example of serializing Serde derived structs to an Excel worksheet using
+//! `rust_xlsxwriter` and the `XlsxSerialize` trait.
+
+use rust_xlsxwriter::{Workbook, XlsxError, XlsxSerialize};
+use serde::Serialize;
+
+fn main() -> Result<(), XlsxError> {
+    let mut workbook = Workbook::new();
+
+    // Add a worksheet to the workbook.
+    let worksheet = workbook.add_worksheet();
+
+    // Create a serializable struct.
+    #[derive(XlsxSerialize, Serialize)]
+    struct Produce {
+        fruit: &'static str,
+
+        #[xlsx(num_format = "$0.00")]
+        cost: f64,
+    }
+
+    // Create some data instances.
+    let items = [
+        Produce {
+            fruit: "Peach",
+            cost: 1.05,
+        },
+        Produce {
+            fruit: "Plum",
+            cost: 0.15,
+        },
+        Produce {
+            fruit: "Pear",
+            cost: 0.75,
+        },
+    ];
+
+    // Set the serialization location and headers.
+    worksheet.set_serialize_headers::<Produce>(0, 0)?;
+
+    // Serialize the data.
+    worksheet.serialize(&items)?;
+
+    // Save the file to disk.
+    workbook.save("serialize.xlsx")?;
+
+    Ok(())
+}
